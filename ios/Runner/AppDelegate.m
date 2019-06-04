18721589a6be89f9f4c7db50c26ecaa010b1ac9d
ReshanMaduka <reshanmaduka615@gmail.com>
2019-06-04 19:49:24 +0530

    add ios folder

--- ios/Runner/AppDelegate.m
+++ ios/Runner/AppDelegate.m
@@ -1,14 +1,11 @@
 #include "AppDelegate.h"
 #include "GeneratedPluginRegistrant.h"
-#import "GoogleMaps/GoogleMaps.h"
-
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application
     didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-      [GMSServices provideAPIKey:@"AIzaSyD9Juj4XYGRnE19y6p6-urYftTDEEafGkU"];
-      [GeneratedPluginRegistrant registerWithRegistry:self];
+  [GeneratedPluginRegistrant registerWithRegistry:self];
   // Override point for customization after application launch.
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
